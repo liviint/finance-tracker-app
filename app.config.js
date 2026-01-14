f568396dd44db050c79e3fc46b5efdcbc2615a69
kevinmm001 <kevinm@teknobuilt.com>
2026-01-14 15:44:03 +0300

    working on the categories functionality

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
