2d3f34f38e3ca2b05dd0f321b6265693e4de80e6
kevinmm001 <kevinm@teknobuilt.com>
2026-01-15 09:25:28 +0300

    added the lock app functionality

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.1",
+    version: "1.0.2",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
