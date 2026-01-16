5198a940c6b1a5bfdd80f1fd72a2c255177f6d42
kevinmm001 <kevinm@teknobuilt.com>
2026-01-16 09:51:15 +0300

    adding the savings tracking functionality

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
