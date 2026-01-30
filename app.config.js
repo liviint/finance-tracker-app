b6486035e995c1c725fcf7ec0b65f857ed0ad3b4
kevinmm001 <kevinm@teknobuilt.com>
2026-01-30 08:45:06 +0300

    worked on the debts syncing

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.8",
+    version: "1.0.9",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
