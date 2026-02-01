4e5af403d8f3f13ad566c678bf2c757130e7c2b2
kevinmm001 <kevinm@teknobuilt.com>
2026-02-01 17:06:00 +0300

    fixed an issue

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.10",
+    version: "1.0.11",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     userInterfaceStyle: "automatic",
