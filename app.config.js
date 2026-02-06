4228ac17cf8c9b26b2e964e649fc7107d1f3d075
kevinmm001 <kevinm@teknobuilt.com>
2026-02-06 11:18:40 +0300

    adding date filters in the transactions

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.13",
+    version: "1.0.14",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     userInterfaceStyle: "automatic",
