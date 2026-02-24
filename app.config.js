bb858af342b248e8c42e93f96fafe340afc1b114
kevinmm001 <kevinm@teknobuilt.com>
2026-02-24 13:36:46 +0300

    firbase stats tracking

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.19",
+    version: "1.0.20",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     userInterfaceStyle: "automatic",
