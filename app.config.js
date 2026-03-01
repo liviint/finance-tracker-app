b41e0fb7ab46e84a27a9f30fd83a7023679e50e1
kevinmm001 <kevinm@teknobuilt.com>
2026-03-01 13:38:09 +0300

    added firebase carsh analytics

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.21",
+    version: "1.0.22",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     userInterfaceStyle: "automatic",
