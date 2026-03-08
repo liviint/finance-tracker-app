8c82dbe8e75501f0b9f0f49f438477c14112212f
kevinmm001 <kevinm@teknobuilt.com>
2026-03-08 15:08:52 +0300

    ordering templates on usage count

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.22",
+    version: "1.0.23",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     userInterfaceStyle: "automatic",
