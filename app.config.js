186e5fbeadc78b12caef23f98f4a2a2f49e7aed7
kevinmm001 <kevinm@teknobuilt.com>
2026-04-21 11:22:10 +0300

    adding google backup

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniamoney",
     name: "ZeniaMoney",
     slug: "zeniamoney",
-    version: "1.0.34",
+    version: "1.0.35",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     userInterfaceStyle: "automatic",
